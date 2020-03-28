9f8284b6623b1c2b71a14b8786aed97cdb203030
Miguel Correia <37122314+Miguel-Correia@users.noreply.github.com>
2020-03-28 23:26:49 +0000

    Adicionou-se as restantes partes da componente Transporte

--- diaAbertoConf/models.py
+++ diaAbertoConf/models.py
@@ -11,11 +11,33 @@ class Transporte(models.Model):
     capacidade = models.IntegerField(db_column='Capacidade', blank=True, null=True)  # Field name made lowercase.
 
     def __str__(self):
-        return self.tipo_transporte
+        return "Transporte nº " + str(self.id)
 
     class Meta:
-        managed = True
+        #managed = True
         db_table = 'transporte'
+
+class HorarioTransporte(models.Model):
+    #id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    hora_de_partida = models.TimeField(db_column='Hora_de_partida', blank=True, null=True)  # Field name made lowercase.
+    Hora_de_chegada = models.TimeField(db_column='Hora_de_chegada', blank=True, null=True)  # Field name made lowercase.
+    #data = models.DateField(db_column='Data', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        #managed = True
+        db_table = 'horario'
+
+class TransporteUniversitarioHorario(models.Model):
+    #id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    horarioid = models.ForeignKey(HorarioTransporte, on_delete = models.CASCADE, db_column='HorarioID')  # Field name made lowercase.
+    transporteid = models.ForeignKey(Transporte, on_delete = models.CASCADE, db_column='TransporteID')  # Field name made lowercase.
+    origem = models.CharField(db_column='Origem', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    destino = models.CharField(db_column='Destino', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    data = models.DateField(db_column='Data', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        #managed = True
+        db_table = 'transporte_universitario_horario'
 
 
 class Ementa(models.Model):
--- diaAbertoConf/views.py
+++ diaAbertoConf/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from diaAbertoConf.models import Transporte
+from diaAbertoConf.models import Transporte, TransporteUniversitarioHorario, HorarioTransporte
 
-from diaAbertoConf.forms import TransporteForm
+from diaAbertoConf.forms import TransporteForm, TransporteUniversitarioHorarioForm, HorarioTransporteForm
 # Create your views here.
 
 def index(request):
     pass
 
+#Transporte CRUD- Create Read Update Delete
 #Creates new transporte
 def createTransporte(request):
     if request.method == "POST":
@@ -32,7 +33,7 @@ def getTransporte(request, id):
     #add later
     return 0
 
-#upadates the fields of a spcific transporte
+#updates the fields of a spcific transporte
 def updateTransporte(request, id):
     dados_Transporte = Transporte.objects.get(id = id)
     form = TransporteForm(request.POST, instance = dados_Transporte)
@@ -47,5 +48,94 @@ def updateTransporte(request, id):
 def deleteTransporte(request, id):
     dados_transporte = Transporte.objects.get(id = id)
     dados_transporte.delete()
+    #add later
+    return 0
+
+#TransporteUniversitario_Horario CRUD- Create Read Update Delete
+#Creates new TransporteUniversitario_Horario, associates Horario to transporte 
+# and defines its Origen and Destino
+def createTransporteUniversitario_Horario(request):
+    if request.method == "POST":
+        form = TransporteUniversitarioHorarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #add later
+            return 0
+        else:
+            form = TransporteForm()
+        #add later
+        return 0
+
+#show all transporteUniversidade_Horarios
+def showTransporteUniversidade_Horarios(request):
+    allTransportesUni_Horario = TransporteUniversitarioHorario.objects.all()
+    #add later
+    return 0
+
+#gets a transporteUniversidade_Horario with a specific id 
+def getTransporteUniversidade_Horario(request, id):
+    dados_TransporteUni_Horario = TransporteUniversitarioHorario.objects.get(id = id)
+    #add later
+    return 0
+
+#upadates the fields of a spcific transporteUniversidade_Horario
+def updateTransporteUniversidade_Horario(request, id):
+    dados_TransporteUni_Horario = TransporteUniversitarioHorario.objects.get(id = id)
+    form = TransporteUniversitarioHorarioForm(request.POST, instance = dados_TransporteUni_Horario)
+    if form.is_valid():
+        form.save()
+        #add later
+        return 0
+    #add later
+    return 0
+
+#deletes a transporteUniversidade_Horario
+def deleteTransporte(request, id):
+    dados_TransporteUni_Horario = TransporteUniversitarioHorario.objects.get(id = id)
+    dados_TransporteUni_Horario.delete()
+    #add later
+    return 0
+
+#Horario Transporte CRUD- Create Read Update Delete
+#Creates new Horario Transporte
+def createHorario_Transporte(request):
+    if request.method == "POST":
+        form = HorarioTransporteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #add later
+            return 0
+        else:
+            form = HorarioTransporteForm()
+        #add later
+        return 0
+
+#show all Hprarios Transporte
+def showHorarios_Transporte(request):
+    allHorarios_Transporte = HorarioTransporte.objects.all()
+    #add later
+    return 0
+
+#gets a Horario_Transporte with a specific id 
+def getHorario_Transporte(request, id):
+    dados_Horario_Transporte = HorarioTransporte.objects.get(id = id)
+    #add later
+    return 0
+
+#updates the fields of a spcific Horario_Transporte
+def updateHorario_Transporte(request, id):
+    dados_Horario_Transporte = HorarioTransporte.objects.get(id = id)
+    form = HorarioTransporteForm(request.POST, instance = dados_Horario_Transporte)
+    if form.is_valid():
+        form.save()
+        #add later
+        return 0
+    #add later
+    return 0
+
+#deletes a Horario_Transporte
+def deleteHorario_Transporte(request, id):
+    dados_Horario_Transporte = Horario_Transporte.objects.get(id = id)
+    dados_Horario_Transporte.delete()
     #add later
     return 0
