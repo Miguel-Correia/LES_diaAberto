0bc26dafd45e5bbcc9eea10966f2b1e1dc7f8eff
Miguel Correia <37122314+Miguel-Correia@users.noreply.github.com>
2020-06-15 20:05:26 +0100

    changed time-zone and language

--- ualgDiaAberto/settings.py
+++ ualgDiaAberto/settings.py
@@ -112,10 +112,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-pt'
 
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'GMT+1'
+    
 USE_I18N = True
 
 USE_L10N = True
