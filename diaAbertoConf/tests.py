9b35bb0dc474b6bfd8e72e90a9a2582315cfc071
BernardoBaltazar <36002439+BernardoBaltazar@users.noreply.github.com>
2020-06-30 18:26:52 +0100

    added more test

--- diaAbertoConf/tests.py
+++ diaAbertoConf/tests.py
@@ -282,13 +282,99 @@ class LoggedInTestCase_Ementa(TestCase):
         uo.save()
         d = Departamento(unidade_organicaid = uo, nome='DEEI')
         d.save()
-        u = Utilizador(unidade_orgânicaid = uo, departamentoid = d, email = 'coordenadorFCT@ualg.pt',nome = 'CoordenadorFCT',data_de_nascimento =  '1999-10-05', numero_telemovel= 99999, user_type= 0b00001, validado=1, password = make_password('12345'))
+        u = Utilizador(unidade_organicaid = uo, departamentoid = d, email = 'coordenadorFCT@ualg.pt',nome = 'CoordenadorFCT',data_de_nascimento =  '1999-10-05', numero_telemovel= 99999, user_type= 0b00001, validado=1, password = make_password('12345'))
         u.save()
         #u.user_permissions.add(Permission.objects.get(name='Can view tarefa'))
-
+        
         self.client.login(email_p='coordenadorFCT@ualg.pt', password_p= '12345')
         response = self.client.get('/GestaoEmentas/')
         self.assertEqual(response.status_code, 403) 
+    def test_enter_createEmentas(self):
+        response = self.client.get('/GestaoEmentas/create')
+        self.assertEqual(response.status_code, 302)
+    
+    def test_enter_createEmentasAuthenticated(self):
+        uo = UnidadeOrganica(nome='FCT')
+        uo.save()
+        d = Departamento(unidade_organicaid = uo, nome='DEEI')
+        d.save()
+        u = Utilizador(unidade_organicaid = uo, departamentoid = d, email = 'coordenadorFCT@ualg.pt',nome = 'CoordenadorFCT',data_de_nascimento =  '1999-10-05', numero_telemovel= 99999, user_type= 0b00001, validado=1, password = make_password('12345'))
+        u.save()
+        #u.user_permissions.add(Permission.objects.get(name='Can add tarefa'))
+        
+        self.client.login(email_p='coordenadorFCT@ualg.pt', password_p= '12345')
+        response = self.client.get('/GestaoEmentas/create')
+        self.assertEqual(response.status_code, 403)
+
+    def test_enter_updateEmentas(self):     
+        e = Ementa(dia='2020-04-23')
+        e.save()
+        
+        response = self.client.get('/GestaoEmentas/update' + str(e.id))
+        self.assertEqual(response.status_code, 302)
+
+    def test_enter_updateEmentasAuthenticated(self):
+        uo = UnidadeOrganica(nome='FCT')
+        uo.save()
+        d = Departamento(unidade_organicaid = uo, nome='DEEI')
+        d.save()
+        u = Utilizador(unidade_organicaid = uo, departamentoid = d, email = 'coordenadorFCT@ualg.pt',nome = 'CoordenadorFCT',data_de_nascimento =  '1999-10-05', numero_telemovel= 99999, user_type= 0b00001, validado=1, password = make_password('12345'))
+        u.save()
+        #u.user_permissions.add(Permission.objects.get(name='Can change tarefa'))
+
+        e = Ementa(dia='2020-04-23')
+        e.save()
+        
+        self.client.login(email_p='coordenadorFCT@ualg.pt', password_p= '12345')
+        response = self.client.get('/GestaoEmentas/update' + str(e.id))
+        self.assertEqual(response.status_code, 403)
+
+    def test_enter_deleteEmentas(self):
+        e = Ementa(dia='2020-04-23')
+        e.save()
+        
+        response = self.client.get('/GestaoEmentas/delete' + str(e.id))
+        self.assertEqual(response.status_code, 302)
+
+    def test_enter_deleteEmentasAuthenticated(self):
+        uo = UnidadeOrganica(nome='FE')
+        uo.save()
+        d = Departamento(unidade_organicaid = uo, nome='A')
+        d.save()
+        u = Utilizador(unidade_organicaid = uo, departamentoid = d, email = 'coordenadorFE@ualg.pt',nome = 'CoordenadorFE',data_de_nascimento =  '1999-10-05', numero_telemovel= 99999, user_type= 0b00001, validado=1, password = make_password('12345'))
+        u.save()
+        #u.user_permissions.add(Permission.objects.get(name='Can delete tarefa'))
+
+        e = Ementa(dia='2020-04-23')
+        e.save()
+        
+        self.client.login(email_p='coordenadorFE@ualg.pt', password_p= '12345')
+        response = self.client.get('/GestaoEmentas/delete' + str(e.id))
+        self.assertEqual(response.status_code, 403)
+
+
+class EmentaForm_TestCase(TestCase):
+
+    def test_form_Valid(self):
+        formEmenta = EmentaForm({'dia': "2020-05-31"})
+        self.assertTrue(formEmenta.is_valid())
+
+    def test_form_not_Valid(self):   
+        formEmenta = EmentaForm({'dia': "Maio"})
+        self.assertFalse(formEmenta.is_valid()) 
+
+    def test_pratoForm_Valid(self):
+        e = Ementa(dia='2020-05-31')
+        e.save()    
+        formPrato = PratoForm({'ementaid': [e.id], 'nome' : "Cenoura", 'tipo': "Sopa", 'descricao' : "F"})
+        self.assertTrue(formPrato.is_valid())
+
+    def test_pratoForm_not_Valid(self):
+        e = Ementa(dia='2020-05-31')
+        e.save()    
+        formPrato = PratoForm({'ementaid': [e.id], 'nome' : 40, 'tipo': "Sopa", 'descricao' : "F"})
+        self.assertTrue(formPrato.is_valid())    
+        
 
 class LoggedInTestCase_DiaAbertoConfig(TestCase):
 
@@ -311,3 +397,61 @@ class LoggedInTestCase_DiaAbertoConfig(TestCase):
         self.client.login(email_p='coordenadorFCT@ualg.pt', password_p= '12345')
         response = self.client.get('/DiaAbertoConf/')
         self.assertEqual(response.status_code, 403)
+
+
+class DiaAbertoForm_TestCase(TestCase):
+
+    def test_form_Valid(self):
+        form = DiaAbertoForm({'titulo': "AA", 
+        'descricao' : "FF", 
+        'email':"DD", 
+        'contacto':112, 
+        'data_inicio':"2020-05-30", 'data_fim':"2020-05-31", 
+        'data_inicio_inscricao': "2020-04-01",'data_fim_inscricao':"2020-04-20", 
+        'data_inicio_propor_atividades' : "2020-02-15",'data_fim_propor_atividades':"2020-03-20",
+        'preco_aluno':3.5,'preco_professor':5.0})
+        self.assertTrue(form.is_valid())
+
+    def test_form_dInicio_After_dFim(self):
+        form = DiaAbertoForm({'titulo': "AA", 
+        'descricao' : "FF", 
+        'email':"DD", 
+        'contacto':112, 
+        'data_inicio':"2020-05-31", 'data_fim':"2020-05-30", 
+        'data_inicio_inscricao': "2020-04-01",'data_fim_inscricao':"2020-04-20", 
+        'data_inicio_propor_atividades' : "2020-02-15",'data_fim_propor_atividades':"2020-03-20",
+        'preco_aluno':3.5,'preco_professor':5.0})
+        self.assertFalse(form.is_valid()) 
+
+    def test_form_dInicInsc_After_dFimInsc(self):
+        form = DiaAbertoForm({'titulo': "AA", 
+        'descricao' : "FF", 
+        'email':"DD", 
+        'contacto':112, 
+        'data_inicio':"2020-05-30", 'data_fim':"2020-05-31", 
+        'data_inicio_inscricao': "2020-04-20",'data_fim_inscricao':"2020-04-01", 
+        'data_inicio_propor_atividades' : "2020-02-15",'data_fim_propor_atividades':"2020-03-20",
+        'preco_aluno':3.5,'preco_professor':5.0})
+        self.assertFalse(form.is_valid())
+
+    def test_form_dIpropAtiv_After_dFpropAtiv(self):
+        form = DiaAbertoForm({'titulo': "AA", 
+        'descricao' : "FF", 
+        'email':"DD", 
+        'contacto':112, 
+        'data_inicio':"2020-05-30", 'data_fim':"2020-05-31", 
+        'data_inicio_inscricao': "2020-04-01",'data_fim_inscricao':"2020-04-20", 
+        'data_inicio_propor_atividades' : "2020-03-20",'data_fim_propor_atividades':"2020-02-20",
+        'preco_aluno':3.5,'preco_professor':5.0})
+        self.assertFalse(form.is_valid()) 
+
+    def test_form_dInicInsc_After_dIpropAtiv(self): 
+        form = DiaAbertoForm({'titulo': "AA", 
+        'descricao' : "FF", 
+        'email':"DD", 
+        'contacto':112, 
+        'data_inicio':"2020-05-31", 'data_fim':"2020-05-32", 
+        'data_inicio_inscricao': "2020-03-20",'data_fim_inscricao':"2020-04-01", 
+        'data_inicio_propor_atividades' : "2020-03-20",'data_fim_propor_atividades':"2020-04-20",
+        'preco_aluno':3.5,'preco_professor':5.0})
+        self.assertFalse(form.is_valid())      
